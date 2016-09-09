786572e8764d1c06db2745efce23262013e5f45d
Michael Müller <mishbag@reu-network.de>
2016-09-09 15:30:12 +0200

    added recording controller

---
+++ src/Controller/RecordingController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class RecordingController extends AppController {
+	
+	public function index()
+	{
+		$this->render();
+	}
+}
+?>
