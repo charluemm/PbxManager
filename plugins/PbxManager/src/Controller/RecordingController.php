212422971770e213d0f27fc84f7a05488e6d4419
Michael Müller <mishbag@reu-network.de>
2016-09-09 22:01:18 +0200

    added controller actions

--- plugins/PbxManager/src/Controller/RecordingController.php
+++ plugins/PbxManager/src/Controller/RecordingController.php
@@ -4,9 +4,27 @@ namespace PbxManager\Controller;
 
 use PbxManager\Controller\AppController;
 
+
+/**
+ * @author Michael Müller <development@reu-network.de>
+ * @author David Howon <howon.david@gmail.com>
+ *
+ */
 class RecordingController extends AppController
 {
 	public function index()
 	{
+	}
+	
+	public function enable($agent = null)
+	{
+		$this->Flash->success("Mithören wurde aktiviert.");
+		return $this->redirect(array('controller' => 'Recording', 'action' => 'index'));
+	}
+	
+	public function disable($agent = null)
+	{
+		$this->Flash->success("Mithören wurde deaktiviert.");
+		return $this->redirect(array('controller' => 'Recording', 'action' => 'index'));
 	}
 }
