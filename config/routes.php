5cc9d0d66cf57f077b09436a5c4c30a2f92275d0
Michael Müller <mishbag@reu-network.de>
2016-11-11 14:21:35 +0100

    modfied router

--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,14 @@
 <?php
+// Router::plugin(
+//     'PbxManager',
+//     ['path' => '/pbx-manager'],
+//     function (RouteBuilder $routes) {
+//         $routes->fallbacks(DashedRoute::class);
+//     }
+// );
 
-Router::plugin(
-    'PbxManager',
-    ['path' => '/pbx-manager'],
-    function (RouteBuilder $routes) {
-        $routes->fallbacks(DashedRoute::class);
-    }
-);
+Router::connect('/pbx-manager',array(
+		'plugin' => 'PbxManager',
+		'controller' => 'Recording',
+		'action' => 'index'
+));
